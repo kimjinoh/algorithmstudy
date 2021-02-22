992e73ce9364591b7a160c9e513b654ea9ae3b56
kimjinoh <wlsdh1110@gmail.com>
2021-02-22 21:57:10 +0900

    level1

--- level1/lv1_완주못한선수.py
+++ level1/lv1_완주못한선수.py
@@ -3,32 +3,50 @@ try:
     completion = list(map(str, input().split()))
 except EOFError as e:
     print(end="")
+##################정렬 후 비교####################
 
+# def solution(participant, completion):
+#     answer = ''
+#     part = sorted(participant)
+#     comp = sorted(completion)
+#     for j in range(len(completion)):
+#         if part[j] != comp[j]:
+#             answer = part[j]
+#             break
+#     if answer == '':
+#         answer = part[-1]
+#     print(answer)
+#     return answer
+# solution(participant, completion)
+##################################################
+###################카운터 객체 사용#################
+# import collections
+# def solution(participant, completion):
+#     answer = collections.Counter(participant) - collections.Counter(completion)
+#     return list(answer.keys())[0]
+# answer = collections.Counter(participant) - collections.Counter(completion)
+# print(answer)
+#######io####
+# stan mimi kiki leo mimi
+# mimi kiki leo stan
+# Counter({'mimi': 1})
+#############
+##################################################
+
+###################해시함수 사용#################
 def solution(participant, completion):
     answer = ''
-    part = sorted(participant)
-    comp = sorted(completion)
-    for j in range(len(completion)):
-        if part[j] != comp[j] :
-            answer = part[j]
-            break
-    if answer == '':
-        answer = part[-1]
-    #count = []
-    # for i in range(len(participant)):
-    #     count.append(0)
-    #     for j in range(i+1, len(participant)):
-    #         if participant[i] == participant[j]:
-    #             count[i] += 1
-    #     for j in range(len(completion)):
-    #         if participant[i] == completion[j]:
-    #             count[i] -= 1
-    #     if count[i] == 0:
-    #         answer = participant[i]
-    # for i in range(len(participant)):
-    #     if participant.count(participant[i]) != completion.count(participant[i]):
-    #         answer = participant[i]
-    #         break
-    print(answer)
+    temp = 0
+    dic = {}
+    for part in participant:
+        dic[hash(part)] = part
+        temp += int(hash(part))
+        print(temp)
+    for com in completion:
+        temp -= hash(com)
+        print(temp)
+    answer = dic[temp]
+    print(dic)
     return answer
 solution(participant, completion)
+##################################################
